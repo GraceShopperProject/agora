1a11d4a4d842e0df2ff1e8d53eccf6035a703535
Jing Jia <nycjing@gmail.com>
2017-09-13 15:51:01 -0400

    update user models, signup form add name, home page greeting change to name

--- server/db/models/user.spec.js
+++ server/db/models/user.spec.js
@@ -8,25 +8,51 @@ const User = db.model('user');
 describe('User model', () => {
   beforeEach(() => db.sync({ force: true }));
 
-  describe('instanceMethods', () => {
-    describe('correctPassword', () => {
-      let cody;
-
-      beforeEach(() => User.create({
-        email: 'cody@puppybook.com',
-        password: 'bones',
-      })
-        .then((user) => {
-          cody = user;
-        }));
-
-      it('returns true if the password is correct', () => {
-        expect(cody.correctPassword('bones')).to.be.equal(true);
+      it('has the expected schema definition', () => {
+          expect(User.attributes.email).to.be.an('object');
       });
 
-      it('returns false if the password is incorrect', () => {
-        expect(cody.correctPassword('bonez')).to.be.equal(false);
+      describe('validations', () => {
+
+          // *Assertion translation*:
+          // The `email` column should be a required field.
+          it('require email', () => {
+              const user = User.build();
+              return user.validate()
+                  .then(() => { throw new Error('Promise should have rejected');})
+                  .catch(err => {
+                      expect(err).to.exist;
+                      expect(err).to.be.an('error');
+                      expect(err.errors).to.contain.a.thing.with.properties({
+                          path: 'email',
+                          type: 'notNull Violation'
+                      });
+                  });
+          });
+
       });
-    }); // end describe('correctPassword')
-  }); // end describe('instanceMethods')
+
+
+
+      describe('instanceMethods', () => {
+          describe('correctPassword', () => {
+            let cody;
+
+            beforeEach(() => User.create({
+              email: 'cody@puppybook.com',
+              password: 'bones',
+            })
+              .then((user) => {
+                cody = user;
+              }));
+
+            it('returns true if the password is correct', () => {
+              expect(cody.correctPassword('bones')).to.be.equal(true);
+            });
+
+            it('returns false if the password is incorrect', () => {
+              expect(cody.correctPassword('bonez')).to.be.equal(false);
+            });
+        }); // end describe('correctPassword')
+    }); // end describe('instanceMethods')
 }); // end describe('User model')
