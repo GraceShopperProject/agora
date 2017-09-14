fd1121b6570090ddbf86c7ab33be1361e4410872
Robin Wilson <robin.kath.wilson@gmail.com>
2017-09-14 14:37:47 -0700

    rwilson: checking status

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -3,18 +3,21 @@ const db = require('../db');
 const User = require('./user');
 
 const Order = db.define('order', {
-  date: {
-    type: Sequelize.DATE,
-    allowNull: false,
-    defaultValue: Sequelize.NOW
-  },
   customizeOrderMessage: {
     type: Sequelize.TEXT,
     allowNull: true
   },
   status: {
     type: Sequelize.ENUM(['Created', 'Processing', 'Cancelled', 'Completed']),
-    allowNull: false
+    allowNull: false,
+    defaultValue: 'Created',
+  },
+  price: { 
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0,
+    },
   }
 });
 
