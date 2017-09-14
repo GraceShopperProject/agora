228813a6242f092aeb51ee9efaf83b6d5e10b2f4
Derrick Beining <derrick.beining@gmail.com>
2017-09-14 14:07:52 -0400

    making progress; mergin master again

--- server/db/seed-derrick.js
+++ server/db/seed-derrick.js
@@ -1,17 +1,68 @@
 
 
 const db = require('./db');
-const dummy = require('faker');
-const {yellow, red} = require('chalk');
+const {
+  Product,
+  User,
+  Order,
+  Review,
+  Category,
+} = require('./models');
 
-const buildNThings = (n, thingBuilder) => {
+const dummy = require('faker');
+
+const { floor, random } = Math;
+
+const pushNThingsIntoArray = (n, thingMaker, uniques) => {
   const things = [];
   while (n) {
-    things.push(thingBuilder());
-    n -= 1;
+    const thing = thingMaker();
+    const isNotDuplicate = uniques.every(attribute => !thing[attribute]);
+    if (isNotDuplicate) {
+      things.push(thing);
+      n -= 1;
+    }
   }
   return things;
 };
+
+
+const buildCategory = () => ({ name: dummy.commerce.productMaterial() });
+const categories = pushNThingsIntoArray(20, buildCategory, ['name']);
+console.log(categories);
+
+
+const buildProduct = () => Product.create({
+  name: dummy.commerce.product(),
+  description: dummy.lorem.paragraphs(),
+  price: dummy.random.number({ min: 1, max: 1000, precision: 2 }),
+  remainingInventory: dummy.random.number({ min: 0, max: 1000, precision: 0 }),
+  categories: [categories[floor(random() * categories.length)]],
+}, {
+  include: [{
+    association: Product.Categories,
+  }],
+});
+
+const products = pushNThingsIntoArray(100, buildProduct);
+
+
+const buildOrder = () => ({
+  status: [
+    'Created',
+    'Processing',
+    'Cancelled',
+    'Completed',
+  ][Math.floor(Math.random() * 4)],
+});
+
+const buildReview = () => ({
+  productId: products[Math.floor(Math.random() * products.length)].id,
+  text: dummy.lorem.paragraphs(),
+  numOfStars: Math.foor(Math.random() * 6),
+});
+
+const reviews = pushNThingsIntoArray();
 
 const buildUser = () => {
   const firstName = dummy.name.firstName();
@@ -33,30 +84,4 @@ const buildUser = () => {
   };
 };
 
-const buildCategory = () => ({name: dummy.commerce.productMaterial()});
-
-const buildProduct = () => ({
-  name: dummy.commerce.product(),
-  description: dummy.lorem.paragraphs(),
-  imgUrl: dummy.image.technics(),
-  price: dummy.commerce.price(),
-  remainingInventory: dummy.random.number(),
-});
-
-const buildOrder = () => ({
-  status: [
-    'Created',
-    'Processing',
-    'Cancelled',
-    'Completed',
-  ][Math.floor(Math.random() * 4)],
-});
-
-const buildReview = () => ({
-  text: dummy.lorem.paragraphs(),
-  numOfStart: Math.foor(Math.random() * 6),
-});
-
-
-const users = buildNThings(50, buildUser());
-const categories = buildNThings
+const users = pushNThingsIntoArray(50, buildUser);
