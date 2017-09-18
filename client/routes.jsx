097adec1f56e6052798c272d123b8a580fd22b19
Derrick Beining <derrick.beining@gmail.com>
2017-09-18 17:45:19 -0400

    corrected some mismatched names

--- client/routes.jsx
+++ client/routes.jsx
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Router } from 'react-router';
-import { Route, Switch } from 'react-router-dom';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Router} from 'react-router';
+import {Route, Switch} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import history from './history';
 
 import {
-  Main,
   NavFooterWrapper,
   Home,
   Login,
@@ -15,9 +14,11 @@ import {
   ShoppingCart,
   OrdersList,
   Category,
-  Productpage,
-  ProductDetailpage,
-  CheckoutForm, ErrorPage, Confirmation,
+  ProductList,
+  ProductDetail,
+  CheckoutForm,
+  ErrorPage,
+  Confirmation,
 } from './components';
 import {
   me,
@@ -31,12 +32,12 @@ import {
  * COMPONENT
  */
 class Routes extends Component {
-  componentDidMount() {
+  componentDidMount () {
     this.props.loadInitialData();
   }
 
-  render() {
-    const { isLoggedIn } = this.props;
+  render () {
+    const {isLoggedIn} = this.props;
 
     return (
       <Router history={history}>
@@ -49,9 +50,9 @@ class Routes extends Component {
             <Route path="/shoppingcart" component={ShoppingCart} />
             <Route path="/checkoutform" component={CheckoutForm} />
             <Route exact path="/category" component={Category} />
-            <Route path="/category/:categoryId" component={Productpage} />
+            <Route path="/category/:categoryId" component={ProductList} />
 
-            <Route path="/products/:productId" component={ProductDetailpage} />
+            <Route path="/products/:productId" component={ProductDetail} />
 
             <Route path="/confirmation" component={Confirmation} />
             <Route path="/error" component={ErrorPage} />
@@ -82,7 +83,7 @@ const mapState = state => ({
 });
 
 const mapDispatch = dispatch => ({
-  loadInitialData() {
+  loadInitialData () {
     dispatch(fetchOrders());
     dispatch(fetchCategories());
     dispatch(getshoppingcart());
