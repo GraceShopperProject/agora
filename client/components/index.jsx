317277bcdc68d9fe8f22d104de2fe2dd31000e81
Derrick Beining <derrick.beining@gmail.com>
2017-09-18 00:56:34 -0400

    formatting

--- client/components/index.jsx
+++ client/components/index.jsx
@@ -3,11 +3,11 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export { default as Main, } from './main';
-export { default as UserHome, } from './user-home';
-export { Login, Signup, } from './auth-form';
+export {default as Main, } from './main';
+export {default as UserHome, } from './user-home';
+export {Login, Signup, } from './auth-form';
 
 export ShoppingCart from './ShoppingCart';
 export Category from './category';
-export { default as OrdersList, } from './orders-list'
+export {default as OrdersList, } from './orders-list'
 export Productpage from './productpage';
--- client/components/main.jsx
+++ client/components/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {connect,} from 'react-redux';
-import {withRouter,Link,} from 'react-router-dom';
-import {logout,} from '../store';
+import { connect } from 'react-redux';
+import { withRouter, Link } from 'react-router-dom';
+import { logout } from '../store';
 
 /**
  * COMPONENT
@@ -11,46 +11,46 @@ import {logout,} from '../store';
  *  rendered out by the component's `children`.
  */
 const Main = (props) => {
-  const {children,handleClick,isLoggedIn,} = props;
+  const { children, handleClick, isLoggedIn } = props;
 
   return (
     <div>
       <h1>Agora</h1>
       <nav>
-          <div className="container">
-              <div id="nav-items" className="collapse navbar-collapse  navbar-left">
-                  <ul className="nav navbar-nav">
-                      {/* The navbar will show these links after you log in */}
-                      <li><Link to="/category">Category</Link></li>
-                  </ul>
-              </div>
-        {
-          isLoggedIn
-            ? <div id="nav-items" className="collapse navbar-collapse  navbar-right">
-                  <ul className="nav navbar-nav">
+        <div className="container">
+          <div id="nav-items" className="collapse navbar-collapse  navbar-left">
+            <ul className="nav navbar-nav">
               {/* The navbar will show these links after you log in */}
-                      <li><Link to="/home">Home</Link></li>
-                      <li><Link to="/orders">Order</Link></li>
-                      <li><a href="#" onClick={handleClick}>Logout</a></li>
-                  </ul>
-            </div>
-            : <div id="nav-items" className="collapse navbar-collapse navbar-right">
-              {/* The navbar will show these links before you log in */}
-                  <ul className="nav navbar-nav">
-                      <li><Link to="/login">Login</Link></li>
-                      <li><Link to="/signup">Sign Up</Link></li>
-                  </ul>
-            </div>
-        }
-
-              <ul className="nav navbar-right">
-                  <li>
-                      <Link to="/shoppingcart" >
-                          <img src="icons8-Shopping Cart-50.png" height="20" width="20" />
-                      </Link>
-                  </li>
-              </ul>
+              <li><Link to="/category">Category</Link></li>
+            </ul>
           </div>
+          {
+            isLoggedIn
+              ? <div id="nav-items" className="collapse navbar-collapse  navbar-right">
+                <ul className="nav navbar-nav">
+                  {/* The navbar will show these links after you log in */}
+                  <li><Link to="/home">Home</Link></li>
+                  <li><Link to="/orders">Order</Link></li>
+                  <li><a href="#" onClick={handleClick}>Logout</a></li>
+                </ul>
+              </div>
+              : <div id="nav-items" className="collapse navbar-collapse navbar-right">
+                {/* The navbar will show these links before you log in */}
+                <ul className="nav navbar-nav">
+                  <li><Link to="/login">Login</Link></li>
+                  <li><Link to="/signup">Sign Up</Link></li>
+                </ul>
+              </div>
+          }
+
+          <ul className="nav navbar-right">
+            <li>
+              <Link to="/shoppingcart" >
+                <img src="icons8-Shopping Cart-50.png" height="20" width="20" />
+              </Link>
+            </li>
+          </ul>
+        </div>
       </nav>
       <hr />
       {children}
@@ -66,14 +66,14 @@ const mapState = state => ({
 });
 
 const mapDispatch = dispatch => ({
-  handleClick () {
+  handleClick() {
     dispatch(logout());
   },
 });
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(connect(mapState,mapDispatch)(Main));
+export default withRouter(connect(mapState, mapDispatch)(Main));
 
 /**
  * PROP TYPES
