955112386d3cd747b12c13212a68dfe23687f094
robin-k-wilson <robin.kath.wilson@gmail.com>
2017-10-25 15:46:36 -0700

    Update index.jsx

--- client/components/common/index.jsx
+++ client/components/common/index.jsx
@@ -1,14 +1,2 @@
 export {default as TileGridFeed} from './TileGridFeed';
-
-
-git mv Layout tempname && git mv tempname layout
-git mv Navigation tempname && git mv tempname navigation;
-git mv Order tempname && git mv tempname order;
-git mv Product tempname && git mv tempname product;
-git mv ShoppingCart tempname && git mv tempname shoppingCart;
-git mv Common tempname && git mv tempname common;
-git mv Trash tempname && git mv tempname trash;
-git mv Admin tempname && git mv tempname admin;
-
-git mv UserAccount tempname && git mv tempname userAccount;
-git mv Authorize tempname && git mv tempname authorize;
+export {default as Form} from './Form';
