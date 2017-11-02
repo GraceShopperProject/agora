d673aa9d05fd83c1e3244928e0f0878c65d00eca
Robin Wilson <robin.kath.wilson@gmail.com>
2017-11-02 14:03:11 -0700

    finished home page

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -18,38 +18,46 @@ class Home extends React.Component {
     console.log(this.props);
     return (
       <div>
-        <section>
-
-          <Jumbotron className="jumbotron-billboard">
-            <div className="img"></div>
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-12">
-                  <Image src="/images/agora_logo2.svg" />
-                  <p>The real you needs comfort.</p>
-                  <p>The real you needs class.</p>
-                  <p>The real you needs Agora.</p>
-                  <p>We have chairs.</p>
+        <div className="subheading-container">
+          <section>
+            <Jumbotron className="jumbotron-billboard">
+              <div className="img"></div>
+              <div className="container">
+                <div className="row">
+                  <div className="col-lg-12">
+                    <Image src="/images/agora_logo2.svg" />
+                    <p>The real you needs comfort.</p>
+                    <p>The real you needs class.</p>
+                    <p>The real you needs Agora.</p>
+                    <p>We have chairs.</p>
+                  </div>
                 </div>
               </div>
+            </Jumbotron>
+          </section>
+
+          <section className="subheadings">
+            <h2>About Agora</h2>
+            <p>Agora, the classiest chair market place in the world. Impress the in-laws, your guests, your children, and even your dog with the classiest of chairs.</p>
+
+            <p>From humble beginnings, Agora started with a chair in a garage. Our founder then started a new mobile app to show off this classy, classy chair. This app went viral with over 1 billion views in one week. Our founder, an entrepreneur and capitalist at heart, decided he wanted to create the largest empire of classy chairs in the world and sell them to you to spread classy chair joy to everyone.</p>
+
+            <p>Please while you browse our selections, remember where Agora came from and don't forget: at Agora, we have chairs.</p>
+          </section>
+          <section className="subheadings">
+            <h2>The Truth About Agora</h2>
+            <p>Agora was created for a E-Commerce website project for Fullstack Academy's remote immersive program.</p>
+            <p>It integrates React, Node, Express, Socket.io, and a PostGres database. We hope you enjoy exploring!</p>
+          </section>
+          <section id="testimonies" className="subheadings">
+            <h2>Testimonies</h2>
+            <div>
+              <p>"Classy."</p>
+              <p>"How so classy."</p>
+              <p>"Wow! They really have chairs!"</p>
             </div>
-          </Jumbotron>
-        </section>
-        <section>
-          <h2>About</h2>
-          <p>Agora was created for a E-Commerce website project for Fullstack Academy's remote immersive program. It integrates React, Node, Express, Socket.io, and a PostGres database. We hope you enjoy exploring!</p>
-        </section>
-        <section>
-          <h2>Testimonies</h2>
-          <p>"Classy."</p>
-          <p>"How so classy."</p>
-          <p>"Wow! They really have chairs!"</p>
-        </section>
-        {
-          (name)
-            ? (<h1>Welcome, {name}</h1>)
-            : (<h1>Welcome to Agora!</h1>)
-        }
+          </section>
+        </div>
       </div>
     );
   }
